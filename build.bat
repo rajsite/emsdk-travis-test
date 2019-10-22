1d0a94ac57bfb1d420babd6d3c396066ec5ab6f0
rajsite <rajsite@users.noreply.github.com>
2019-10-21 20:00:27 -0500

    Add build script and vscode config

---
+++ build.bat
@@ -0,0 +1,2 @@
+call ..\emsdk\emsdk_env.bat
+emcc src\hello_world.c -o dist\a.out.js
--- src/hello_world.c
+++ src/hello_world.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
+#include<emscripten.h>
+
+// int main() {
+//   printf("Hello to the entire HTML world!\n");
+//   double test = 7;
+//   // EM_ASM({
+//   //   console.log('I received ' + $0);
+//   // }, test);
+//   return 0;
+// }
+
+EM_JS(void, read_data, (int* data), {
+  debugger;
+  console.log('Data: ' + HEAP32[data>>2] + ', ' + HEAP32[(data+4)>>2]);
+});
 
 int main() {
-  printf("Hello to the entire HTML world!\n");
+  int arr[2] = { 30, 45 };
+  read_data(arr);
   return 0;
 }
-
